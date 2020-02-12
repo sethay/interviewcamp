909b8b26378f9cdf0a32d83993fca093efdf07ee
sethay <ayushseth12@gmail.com>
2020-02-13 03:51:06 +0530

    Adding Solutions

---
+++ src/main/java/com/sethay/interviewcamp/solutions/week1/hashmap/Question4.java
@@ -0,0 +1,24 @@
+package com.sethay.interviewcamp.solutions.week1.hashmap;
+
+public class Question4 {
+  // Given a sorted array in non-decreasing order, return an array of squares of each number, also
+  // in non-decreasing order. For example:
+  // [-4,-2,-1,0,3,5] -> [0,1,4,9,16,25]
+
+  public static void main(String[] args){
+
+      int[] arr=new int[]{-4,-2,-1,0,3,5};
+
+
+
+
+
+
+
+  }
+
+
+
+
+}
+
